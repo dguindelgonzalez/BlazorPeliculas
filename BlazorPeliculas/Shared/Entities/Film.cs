514569d76cae2c572962097b021b60de9c723ef6
Daniel <dguindelgonzalez-sobremesa@users.noreply.github.com>
2020-07-02 12:59:52 +0200

    Introduction to Razor

---
+++ BlazorPeliculas/Shared/Entities/Film.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPeliculas.Shared.Entities
+{
+    public class Film
+    {
+        public string Title { get; set; }
+        public DateTime Launch { get; set; }
+    }
+}
